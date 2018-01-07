12c01af05a94cb1b88629f7ddcaac4457a98bac3
Matteo Collina <hello@matteocollina.com>
2018-01-07 12:27:29 +0100

    forward POST

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const fp = require('fastify-plugin')
 const http = require('http')
 const URL = require('url').URL
 
+// TODO add a URL object cache
+
 module.exports = fp(function (fastify, opts, next) {
   const agent = new http.Agent({
     keepAlive: true,
@@ -13,27 +15,28 @@ module.exports = fp(function (fastify, opts, next) {
   })
 
   fastify.decorateReply('forward', function (dest) {
+    const req = this.request.req
     const url = new URL(dest)
-    url.method = 'GET'
-    url.agent = agent
-    const internal = http.request(url)
-    internal.end() // TODO support posts
+    const opts = {
+      method: req.method,
+      port: url.port,
+      hostname: url.hostname,
+      headers: req.headers,
+      agent: agent
+    }
+
+    // TODO support HTTPS
+    const internal = http.request(opts)
+
+    // TODO support different content-types
+    internal.end(JSON.stringify(this.request.body))
 
     // TODO what about trailers?
     internal.on('error', (err) => {
       this.send(err)
     })
     internal.on('response', (res) => {
-      const headers = res.headers
-      const headersKeys = Object.keys(headers)
-
-      var i
-      var header
-
-      for (i = 0; i < headersKeys.length; i++) {
-        header = headersKeys[i]
-        this.header(header, headers[header])
-      }
+      copyHeaders(res, this)
 
       this
         .code(res.statusCode)
@@ -50,3 +53,16 @@ module.exports = fp(function (fastify, opts, next) {
 
   next()
 }, '>= 0.37.0')
+
+function copyHeaders (res, reply) {
+  const headers = res.headers
+  const headersKeys = Object.keys(headers)
+
+  var i
+  var header
+
+  for (i = 0; i < headersKeys.length; i++) {
+    header = headersKeys[i]
+    reply.header(header, headers[header])
+  }
+}
--- test.js
+++ test.js
@@ -6,8 +6,8 @@ const Forward = require('.')
 const http = require('http')
 const get = require('simple-get').concat
 
-test('forward a request', (t) => {
-  t.plan(8)
+test('forward a GET request', (t) => {
+  t.plan(9)
 
   const instance = Fastify()
   instance.register(Forward)
@@ -16,6 +16,7 @@ test('forward a request', (t) => {
 
   const target = http.createServer((req, res) => {
     t.pass('request proxied')
+    t.equal(req.method, 'GET')
     res.statusCode = 205
     res.setHeader('Content-Type', 'text/plain')
     res.setHeader('x-my-header', 'hello!')
@@ -40,6 +41,58 @@ test('forward a request', (t) => {
         t.equal(res.headers['x-my-header'], 'hello!')
         t.equal(res.statusCode, 205)
         t.equal(data.toString(), 'hello world')
+      })
+    })
+  })
+})
+
+test('forward a POST request', (t) => {
+  t.plan(8)
+
+  const instance = Fastify()
+  instance.register(Forward)
+
+  t.tearDown(instance.close.bind(instance))
+
+  const target = http.createServer((req, res) => {
+    t.pass('request proxied')
+    t.equal(req.method, 'POST')
+    t.equal(req.headers['content-type'], 'application/json')
+    var data = ''
+    req.setEncoding('utf8')
+    req.on('data', (d) => {
+      data += d
+    })
+    req.on('end', () => {
+      t.deepEqual(JSON.parse(data), { hello: 'world' })
+      res.statusCode = 200
+      res.setHeader('content-type', 'application/json')
+      res.end(JSON.stringify({ something: 'else' }))
+    })
+  })
+
+  instance.post('/', (request, reply) => {
+    reply.forward(`http://localhost:${target.address().port}`)
+  })
+
+  t.tearDown(target.close.bind(target))
+
+  instance.listen(0, (err) => {
+    t.error(err)
+
+    target.listen(0, (err) => {
+      t.error(err)
+
+      get({
+        url: `http://localhost:${instance.server.address().port}`,
+        method: 'POST',
+        json: true,
+        body: {
+          hello: 'world'
+        }
+      }, (err, res, data) => {
+        t.error(err)
+        t.deepEqual(data, { something: 'else' })
       })
     })
   })
