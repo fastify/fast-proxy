8f0f9da4bbb202d3927fac70159f4a1ba42899f5
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-05-23 11:05:59 +0200

    removing bodyparser from demo

--- demos/gateway.js
+++ demos/gateway.js
@@ -1,10 +1,6 @@
-const bodyParser = require('body-parser')
 const { proxy } = require('../index')({})
 
 const service = require('restana')()
-service.use(bodyParser.json())
-service.use(bodyParser.text())
-
 service.all('/service/*', function (req, res) {
   proxy(req, res, 'http://127.0.0.1:3000' + req.url, {})
 })
