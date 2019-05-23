67485d4800a9039bc093e851ab4a7a1ce8fd71f4
Rolando Santamaria Maso <kyberneees@gmail.com>
2019-05-23 10:16:17 +0200

    adding text/plain parser

--- demos/gateway.js
+++ demos/gateway.js
@@ -3,6 +3,7 @@ const { proxy } = require('../index')({})
 
 const service = require('restana')()
 service.use(bodyParser.json())
+service.use(bodyParser.text())
 
 service.all('/service/*', function (req, res) {
   proxy(req, res, 'http://127.0.0.1:3000' + req.url, {})
