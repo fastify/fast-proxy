7324b362111c82007d1e0c10c84133adfcf9dadd
Trygve Lie <post@trygve-lie.com>
2020-03-03 11:53:29 +0100

    Do not throw when body is an object but not application/json

--- index.js
+++ index.js
@@ -34,11 +34,8 @@ module.exports = (opts) => {
       }
 
       // we leverage caching to avoid parsing the destination URL
-      let url = cache.get(source)
-      if (!url) {
-        url = new URL(source, base)
-        cache.set(source, url)
-      }
+      const url = cache.get(source) || new URL(source, base)
+      cache.set(source, url)
 
       const sourceHttp2 = req.httpVersionMajor === 2
       const headers = { ...sourceHttp2 ? filterPseudoHeaders(req.headers) : req.headers }
@@ -49,26 +46,30 @@ module.exports = (opts) => {
 
       let body = ''
 
+      // Per RFC 7231 §3.1.1.5 if this header is not present we MAY assume application/octet-stream
+      const contentType = req.headers['content-type'] || 'application/octet-stream'
+
       // according to https://tools.ietf.org/html/rfc2616#section-4.3
-      // proxy should ignore message body when it's a GET or HEAD request
+      // fastify ignore message body when it's a GET or HEAD request
       // when proxy this request, we should reset the content-length to make it a valid http request
+      // discussion: https://github.com/fastify/fastify/issues/953
       if (req.method === 'GET' || req.method === 'HEAD') {
         headers['content-length'] = 0
       } else {
         if (req.body) {
           if (req.body instanceof Stream) {
             body = req.body
-          } else {
-            // Per RFC 7231 §3.1.1.5 if this header is not present we MAY assume application/octet-stream
-            const contentType = req.headers['content-type'] || 'application/octet-stream'
-            // detect if body should be encoded as JSON
-            // supporting extended content-type header formats:
-            // - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Type
-            const shouldEncodeJSON = contentType.toLowerCase().indexOf('application/json') === 0
-            // transparently support JSON encoding
-            body = shouldEncodeJSON ? JSON.stringify(req.body) : req.body
-            // update origin request headers after encoding
+
+          // detect if body should be encoded as JSON
+          // supporting extended content-type header formats:
+          // - https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Type
+          } else if (contentType.toLowerCase().indexOf('application/json') === 0 || typeof req.body === 'object') {
+            body = JSON.stringify(req.body)
             headers['content-length'] = Buffer.byteLength(body)
+            headers['content-type'] = contentType
+          } else {
+            body = req.body
+            headers['content-length'] = body.length
             headers['content-type'] = contentType
           }
         } else {
